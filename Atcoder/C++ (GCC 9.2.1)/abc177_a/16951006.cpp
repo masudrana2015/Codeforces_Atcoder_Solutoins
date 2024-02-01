169be800b9895285ae2c294c8b782b66c79ac043
Masud Rana <97425602+masudrana2015@users.noreply.github.com>
2024-02-02 00:53:23 +0600

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc177_a/16951006.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+int main()
+{
+    ll m,n,x;
+double t;
+cin>>m>>n>>x;
+t =(double) m/x;
+if(t <= n)
+{
+  cout<<"Yes\n";
+}
+else
+{
+cout<<"No\n";
+}
+}
