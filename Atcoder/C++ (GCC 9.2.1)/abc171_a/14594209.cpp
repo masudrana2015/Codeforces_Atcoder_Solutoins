c563b6b5fb55d715b54e30493ea048d5284bf2ca
Masud Rana <97425602+masudrana2015@users.noreply.github.com>
2024-02-02 00:53:44 +0600

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc171_a/14594209.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+int main()
+{
+    char ch;
+    cin>>ch;
+    if(ch >= 'A' && ch <= 'Z')
+    {
+        cout<<"A\n";
+    }
+    else
+    {
+        cout<<"a\n";
+    }
+
+}
