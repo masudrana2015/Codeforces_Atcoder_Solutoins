0eb32c1c1c59fe7dcd71153e1e190ec81ebfa07d
Masud Rana <97425602+masudrana2015@users.noreply.github.com>
2024-02-02 00:53:56 +0600

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc164_a/12336121.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+int main()
+{
+    ll n,m,k=0,g,x,f,t;
+    cin>>m>>n;
+    if(m > n)
+    {
+        cout<<"safe\n";
+    }
+    else
+    {
+        cout<<"unsafe\n";
+    }
+
+}
