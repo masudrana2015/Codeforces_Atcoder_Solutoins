f5dd795afb328426197a90564e7b36a9e60b595d
Masud Rana <97425602+masudrana2015@users.noreply.github.com>
2024-02-02 00:54:09 +0600

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc163_a/12095525.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+int main()
+{
+    ll n,g=0,f=0,k,l,c=0,x,t=0;
+    double r = 3.14159265358979323846,m;
+    cin>>n;
+    m = 2*r*n;
+    cout<<m<<endl;
+
+}
