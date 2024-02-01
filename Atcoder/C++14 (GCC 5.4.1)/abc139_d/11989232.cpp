21776f1f7e1a032216fd0865ee265e67372ba0e4
Masud Rana <97425602+masudrana2015@users.noreply.github.com>
2024-02-02 00:54:15 +0600

    Committing files

---
+++ Atcoder/C++14 (GCC 5.4.1)/abc139_d/11989232.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+int main()
+{
+    ll n,c=0,t=0,b,s=0,f,g,d=0,x=0;
+    cin>>n;
+    f = n-1;
+    g = n*f;
+    g/=2;
+    cout<<g<<endl;
+}
